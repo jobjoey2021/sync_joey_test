f519f0c2a324bcf48c318f93769d1225c7268b70
jobjoey2021 <84551590+jobjoey2021@users.noreply.github.com>
2022-01-06 16:41:39 +0800

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,58 @@
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+<h1>Hello Vanilla!</h1>
+
+同步ng 12/31 這就是 coding 的難處
+<h2>  joey codesandbox in NB chrome 2021/12/27</h2>
+<h2>  同步算成功了 2021/12/19</h2>
+<h2>可以用 iPad 在 bed coding 了</h2>
+<h2>iPad; play.js; codeSandbox win chrome; github; vscode; netlify; apps html;
+</h2>
+<div>
+  We use the same configuration as Parcel to bundle this sandbox, you can find more
+  info about Parcel 
+
+
+  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
+</div>
+
+<table>
+  <tr>
+    <th>Company</th>
+    <th>Contact</th>
+    <th>Country</th>
+  </tr>
+  <tr>
+    <td>Alfreds Futterkiste</td>
+    <td>Maria Anders</td>
+    <td>Germany</td>
+  </tr>
+  <tr>
+    <td>Centro comercial Moctezuma</td>
+    <td>Francisco Chang</td>
+    <td>Mexico</td>
+  </tr>
+  <tr>
+    <td>Ernst Handel</td>
+    <td>Roland Mendel</td>
+    <td>Austria</td>
+  </tr>
+  <tr>
+    <td>Island Trading</td>
+    <td>Helen Bennett</td>
+    <td>UK</td>
+  </tr>
+  <tr>
+    <td>Laughing Bacchus Winecellars</td>
+    <td>Yoshi Tannamuri</td>
+    <td>Canada</td>
+  </tr>
+  <tr>
+    <td>Magazzini Alimentari Riuniti</td>
+    <td>Giovanni Rovelli</td>
+    <td>Italy</td>
+  </tr>
+</table>
+
+`;
